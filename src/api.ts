3ed51f1b5c828c60736a530a182d54b78cde7d1e
batunpc <batuhnpc@gmail.com>
2022-10-15 04:00:21 -0400

    use send not json - as suggested from feedback

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:8080';
+const apiUrl = process.env.API_URL || "http://localhost:8080";
 
 /**
  * Given an authenticated user, request all fragments for this user from the
@@ -8,58 +8,99 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
  */
 
 export async function getUserFragments(user: any) {
-	console.log('Requesting user fragments data...');
-	try {
-		const res = await fetch(`${apiUrl}/v1/fragments`, {
-			headers: {
-				Authorization: user.authorizationHeaders().Authorization
-			}
-		});
-		if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+  console.log("Requesting user fragments data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      headers: {
+        Authorization: user.authorizationHeaders().Authorization,
+      },
+    });
+    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
 
-		const data = await res.json();
+    const data = await res.json();
 
-		console.log('Got user fragments data', { fragments: data });
-		return { fragments: data };
-	} catch (err) {
-		console.error('Unable to call GET /v1/fragments', { err });
-	}
+    console.log("Got user fragments data", { fragments: data });
+    return { fragments: data };
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragments", { err });
+  }
 }
 
+// export async function getFragmentById(user: any, id: string) {
+//   console.log(`getFragmentById : ${id}`);
+//   try {
+//     const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+//       headers: {
+//         Authorization: user.authorizationHeaders().Authorization,
+//       },
+//     });
+//     if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+//     const contentType = res.headers.get("Content-Type");
+
+//     if (contentType === "application/json") {
+//       const data = await res.json();
+//       console.log("Got fragment data", { fragment: data });
+//     } else if (contentType === "text/plain") {
+//       const data = await res.text();
+//       console.log("Got fragment data", { fragment: data });
+//     }
+//   } catch (err: any) {
+//     console.error("Unable to call GET /v1/fragment/:id", { err });
+//   }
+// }
 export async function getFragmentById(user: any, id: string) {
-	console.log(`getFragmentById : ${id}`);
-	try {
-		const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
-			headers: {
-				Authorization: user.authorizationHeaders().Authorization
-			}
-		});
-		if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-		const data = await res.json();
-		console.log('Got user fragments data', { fragments: data });
-		return { fragments: data };
-	} catch (err: any) {
-		console.error('Unable to call GET /v1/fragment/:id', { err });
-	}
+  console.log("Requesting fragment data...");
+
+  const dataContainer = document.querySelector("#data");
+  const image = document.querySelector("#image");
+  let data;
+
+  // clear data before getting new data
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      headers: user.authorizationHeaders(),
+    });
+
+    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+
+    const contentType = res.headers.get("Content-Type");
+    console.log(contentType);
+    if (contentType === "application/json; charset=utf-8") {
+      const data = await res.json();
+      //data.textContent = JSON.stringify(data, null, 4);
+      console.log("Got fragment data", JSON.stringify(data, null, 4));
+      return data;
+    } else if (contentType === "text/plain; charset=utf-8") {
+      console.log("text/plain");
+      data = await res.text();
+      console.log("Got fragment data", data);
+      return data;
+    }
+
+    console.log("Got user fragment data", { data });
+  } catch (err) {
+    console.error(`Unable to call GET /v1/fragments/${id}`, { err });
+  }
 }
 // user, contentType, data
 export async function postFragment(user: any, contentType: string, value: any) {
-	console.log('Creating fragment...');
+  console.log("Creating fragment...");
 
-	try {
-		const res = await fetch(`${apiUrl}/v1/fragments`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': contentType,
-				Authorization: user.authorizationHeaders().Authorization
-			},
-			body: value
-		});
-		if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-		const data = await res.json();
-		return { fragments: data };
-	} catch (err: Error | any) {
-		console.error('Unable to call POST /v1/fragment', { err: err.message });
-		throw new Error('Unable to call POST /v1/fragment');
-	}
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": contentType,
+        Authorization: user.authorizationHeaders().Authorization,
+      },
+      body: value,
+    });
+    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+    const data = await res.json();
+    return { fragments: data };
+  } catch (err: Error | any) {
+    console.error("Unable to call POST /v1/fragment", { err: err.message });
+    throw new Error("Unable to call POST /v1/fragment");
+  }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,117 +1,106 @@
-import { Auth, getUser } from './auth';
-import { getUserFragments, getFragmentById, postFragment } from './api';
-import { ErrorMessages, isError } from './error';
+import { Auth, getUser } from "./auth";
+import { getUserFragments, getFragmentById, postFragment } from "./api";
+import { ErrorMessages, isError } from "./error";
 
 async function init() {
-	// Get our UI elements
-	const userSection = document.querySelector('#user');
-	const loginBtn = document.querySelector('#login');
-	const logoutBtn = document.querySelector('#logout');
-	const fragmentForm = document.querySelector('form');
+  // Get our UI elements
+  const userSection = document.querySelector("#user");
+  const loginBtn = document.querySelector("#login");
+  const logoutBtn = document.querySelector("#logout");
+  const fragmentForm = document.querySelector("form");
 
-	// See if we're signed in (i.e., we'll have a `user` object)
-	const user = await getUser();
-	// Wire up event handlers to deal with login and logout.
-	loginBtn
-		? loginBtn.addEventListener('click', () => Auth.federatedSignIn())
-		: null;
+  // See if we're signed in (i.e., we'll have a `user` object)
+  const user = await getUser();
+  // Wire up event handlers to deal with login and logout.
+  loginBtn
+    ? loginBtn.addEventListener("click", () => Auth.federatedSignIn())
+    : null;
 
-	logoutBtn
-		? logoutBtn.addEventListener('click', () => Auth.signOut())
-		: null;
+  logoutBtn ? logoutBtn.addEventListener("click", () => Auth.signOut()) : null;
 
-	if (!user) {
-		// Disable the Logout button
-		logoutBtn?.setAttribute('disabled', 'true');
-		return;
-	}
-	// Log the user info for debugging purposes
-	console.log({ user });
-	// Update the UI to welcome the user
-	userSection?.attributes.removeNamedItem('hidden');
-	loginBtn?.setAttribute('disabled', 'true');
+  if (!user) {
+    // Disable the Logout button
+    logoutBtn?.setAttribute("disabled", "true");
+    return;
+  }
+  // Log the user info for debugging purposes
+  console.log({ user });
+  // Update the UI to welcome the user
+  userSection?.attributes.removeNamedItem("hidden");
+  loginBtn?.setAttribute("disabled", "true");
 
-	userSection
-		?.querySelector('.username')
-		?.appendChild(document.createTextNode(user.username));
-	loginBtn?.classList.add('hidden');
+  userSection
+    ?.querySelector(".username")
+    ?.appendChild(document.createTextNode(user.username));
+  loginBtn?.classList.add("hidden");
 
-	// Do an authenticated request to the fragments API server and log the result
-	getUserFragments(user); // api call
+  // Do an authenticated request to the fragments API server and log the result
+  getUserFragments(user); // api call
 
-	// ========================================================
-	// FRAGMENT FORM
-	fragmentForm?.addEventListener('submit', fragmentEndpoints);
-	async function fragmentEndpoints(e: Event) {
-		e.preventDefault();
+  // ========================================================
+  // FRAGMENT FORM
+  fragmentForm?.addEventListener("submit", fragmentEndpoints);
+  async function fragmentEndpoints(e: Event) {
+    e.preventDefault();
 
-		// value of whatever typed into textarea
-		const inputValue = (<HTMLInputElement>(
-			document.getElementById('textFragment')
-		)).value;
+    // value of whatever typed into textarea
+    const inputValue = (<HTMLInputElement>(
+      document.getElementById("textFragment")
+    )).value;
 
-		// == POST - Create a fragment ==
-		const newFragment = await postFragment(user, 'text/plain', inputValue);
-		if (isError(newFragment)) {
-			if (newFragment.message === ErrorMessages.postFragmentError) {
-				console.log(
-					`Returned POST fragment error ${JSON.stringify(
-						newFragment
-					)}`
-				);
-			} else {
-				console.log(
-					`Returned POST fragment error ${JSON.stringify(
-						newFragment
-					)}`
-				);
-			}
-		} else {
-			console.log(
-				`Returned POST fragment ${JSON.stringify(newFragment)}`
-			);
-		}
-		// == GET - return all fragments ==
-		const fragment = await getUserFragments(user);
-		if (isError(fragment)) {
-			if (fragment.message === ErrorMessages.getUserFragmentsError) {
-				console.log(
-					`Returned GET fragment error ${JSON.stringify(fragment)}`
-				);
-			} else {
-				console.log(
-					`Returned GET fragment error ${JSON.stringify(fragment)}`
-				);
-			}
-		} else {
-			console.log(`Returned GET fragment ${fragment}`);
-		}
+    // == POST - Create a fragment ==
+    const newFragment = await postFragment(user, "text/plain", inputValue);
+    if (isError(newFragment)) {
+      if (newFragment.message === ErrorMessages.postFragmentError) {
+        console.log(
+          `Returned POST fragment error ${JSON.stringify(newFragment)}`
+        );
+      } else {
+        console.log(
+          `Returned POST fragment error ${JSON.stringify(newFragment)}`
+        );
+      }
+    } else {
+      console.log(`Returned POST fragment ${JSON.stringify(newFragment)}`);
+    }
+    // == GET - return all fragments ==
+    const fragment = await getUserFragments(user);
+    if (isError(fragment)) {
+      if (fragment.message === ErrorMessages.getUserFragmentsError) {
+        console.log(`Returned GET fragment error ${JSON.stringify(fragment)}`);
+      } else {
+        console.log(`Returned GET fragment error ${JSON.stringify(fragment)}`);
+      }
+    } else {
+      console.log(
+        `Returned GET fragment ${JSON.stringify(fragment?.fragments)}`
+      );
+    }
 
-		// Total number of fragments
-		const totalLength = fragment?.fragments.data.length;
-		// The newly created fragment Id
-		const newFragmentId = fragment?.fragments.data[totalLength - 1];
+    // Total number of fragments
+    const totalLength = fragment?.fragments.data.length;
+    // The newly created fragment Id
+    const newFragmentId = fragment?.fragments.data[totalLength - 1];
 
-		// List of fragment Ids
-		const fragmentList = document.querySelector('#fragmentList');
-		const fragmentListItem = document.createElement('li');
-		fragmentListItem.classList.add('fragment');
-		const dspId = `Fragment ID: ${newFragmentId}`;
+    // List of fragment Ids
+    const fragmentList = document.querySelector("#fragmentList");
+    const fragmentListItem = document.createElement("li");
+    fragmentListItem.classList.add("fragment");
+    const dspId = `Fragment ID: ${newFragmentId}`;
 
-		//fragmentListItem.appendChild(document.createTextNode(newFragmentId));
-		fragmentListItem.appendChild(document.createTextNode(dspId));
+    //fragmentListItem.appendChild(document.createTextNode(newFragmentId));
+    fragmentListItem.appendChild(document.createTextNode(dspId));
 
-		fragmentList?.appendChild(fragmentListItem);
+    fragmentList?.appendChild(fragmentListItem);
 
-		// == GET - return a specific fragment by ID==
-		const fragmentById = await getFragmentById(user, newFragmentId);
-		const data = fragmentById?.fragments.data;
-		const dspData = `Fragment Data: ${data}`;
-		// append data to fragmentList
-		fragmentList?.appendChild(document.createTextNode(dspData));
-		console.log(`Returned GET fragment by id ${data}`);
-	}
+    // == GET - return a specific fragment by ID==
+    const fragmentById = await getFragmentById(user, newFragmentId);
+    const dspData = `Fragment Data: ${JSON.stringify(fragmentById)}`;
+    // append data to fragmentList
+    fragmentList?.appendChild(document.createTextNode(dspData));
+    console.log(`Returned GET fragment by id${fragmentById}`);
+  }
 }
 
 // Wait for the DOM to be ready, then start the app
-addEventListener('DOMContentLoaded', init);
+addEventListener("DOMContentLoaded", init);
